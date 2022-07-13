b828f06f30fb12c7814cc4b2fc632942e8273578
Julie <julie@bitcoin.com>
2022-07-13 14:32:51 +0900

    split out addHeaders function

---
+++ addHeaders.ts
@@ -0,0 +1,49 @@
+import { Request } from "@cloudflare/workers-types";
+
+import {
+  DEFAULT_SECURITY_HEADERS,
+  BLOCKED_HEADERS,
+  CORS_HEADERS,
+} from "./constants";
+
+export async function addHeaders(req: Request, response: Response) {
+  // let response = await fetch(req);
+  let newHeaders = new Headers(response.headers);
+
+  const tlsVersion = req.cf.tlsVersion;
+  // This sets the headers for HTML responses:
+  if (
+    newHeaders.has("Content-Type") &&
+    !newHeaders.get("Content-Type")?.includes("text/html")
+  ) {
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: newHeaders,
+    });
+  }
+
+  Object.keys(DEFAULT_SECURITY_HEADERS).map(function (name) {
+    newHeaders.set(name, DEFAULT_SECURITY_HEADERS[name]);
+  });
+
+  Object.keys(CORS_HEADERS).map(function (name) {
+    newHeaders.set(name, CORS_HEADERS[name]);
+  });
+
+  BLOCKED_HEADERS.forEach(function (name) {
+    newHeaders.delete(name);
+  });
+
+  if (tlsVersion !== "TLSv1.2" && tlsVersion !== "TLSv1.3") {
+    return new Response("You need to use TLS version 1.2 or higher.", {
+      status: 400,
+    });
+  } else {
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: newHeaders,
+    });
+  }
+}
