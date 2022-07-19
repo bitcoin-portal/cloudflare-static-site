1c8dc333e799c60c7b1d32856f370f591ccd548c
Julie <julie@bitcoin.com>
2022-07-19 11:32:50 +0900

    comment out KVNamespace

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -4,6 +4,5 @@ declare global {
   const CORS: string;
   const REACT_ROUTING: "true" | "false";
   const REDIRECT: string;
-  // const myKVNamespace: KVNamespace;
-  const ASSET_NAMESPACE: string;
+  // const name: KVNamespace;
 }
